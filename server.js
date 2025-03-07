020c3b429abdefdf1f937030427338a1a3ff2991
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-03-07 12:39:32 +0100

    cleanup express config (use same config as ironlauncher)

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const app = require("./app");
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const cors = require("cors");
+
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
